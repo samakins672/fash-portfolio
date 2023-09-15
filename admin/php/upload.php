0db5a2d5d3a6873ef8e9f8d22315577f9d1dbd03
Samuel Akins <akinyemisamuel170@gmail.com>
2023-09-15 08:00:00 +0100

    fix function definition

--- admin/php/upload.php
+++ admin/php/upload.php
@@ -4,6 +4,22 @@ require '../../../config/db.php';
 
 $conn = mysqli_connect("localhost", DB_USERNAME, DB_PASSWORD, "sannexng_fashshotit");
 
+// This function will return a random
+// string of specified length
+function random_strings($length_of_string)
+{
+  // String of all alphanumeric character
+  $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+  // Shuffle the $str_result and returns substring
+  // of specified length
+  return substr(
+    str_shuffle($str_result),
+    0,
+    $length_of_string
+  );
+}
+
 $created_by = $_SESSION['fash_admin_id'];
 $files = array_filter($_FILES['pictures']['name']);
 
